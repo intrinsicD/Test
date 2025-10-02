2596cef3744f0f975cd55f104370ebe74cbff143
dieckman <dieckman@cs.uni-bonn.de>
2025-10-02 09:52:33 +0200

    updated triangle

--- engine/geometry/include/engine/geometry/shapes/triangle.hpp
+++ engine/geometry/include/engine/geometry/shapes/triangle.hpp
@@ -21,31 +21,56 @@ namespace engine::geometry {
         math::vec3 c;
     };
 
-    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 Normal(const Triangle &t) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 Normal(const Triangle &triangle) noexcept;
 
-    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 UnitNormal(const Triangle &t) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 UnitNormal(const Triangle &triangle) noexcept;
 
-    [[nodiscard]] ENGINE_GEOMETRY_API double Area(const Triangle &t) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API double Area(const Triangle &triangle) noexcept;
 
-    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 Centroid(const Triangle &t) noexcept;
-
-    //------------------------------------------------------------------------------------------------------------------
-
-    [[nodiscard]] ENGINE_GEOMETRY_API bool Contains(const Triangle &t, const math::vec3 &point) noexcept;
-    [[nodiscard]] ENGINE_GEOMETRY_API bool Contains(const Triangle &t, const Segment &segment) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 Centroid(const Triangle &triangle) noexcept;
 
     //------------------------------------------------------------------------------------------------------------------
 
-    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &t, const Aabb &aabb) noexcept;
-    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &t, const Cylinder &cylinder) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Contains(const Triangle &outer, const math::vec3 &inner) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Contains(const Triangle &outer, const Triangle &inner) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Contains(const Triangle &outer, const Segment &inner) noexcept;
+
     //------------------------------------------------------------------------------------------------------------------
 
-    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 ClosestPoint(const Triangle &t, const math::vec3 &point) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Aabb &box) noexcept;
 
-    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 ToBarycentricCoords(const Triangle &t, const math::vec3 &point) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Obb &box) noexcept;
 
-    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 FromBarycentricCoords(const Triangle& t, const math::vec3& bc) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Sphere &sphere) noexcept;
 
-    [[nodiscard]] ENGINE_GEOMETRY_API double
-    SquaredDistance(const Triangle& triangle, const math::vec3& point) noexcept;
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Cylinder &cylinder) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Ellipsoid &ellipsoid) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Line &line) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Plane &plane) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Ray &ray) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Segment &segment) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API bool Intersects(const Triangle &triangle, const Triangle &other) noexcept;
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 ClosestPoint(const Triangle &triangle,
+                                                              const math::vec3 &point) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 ToBarycentricCoords(const Triangle &triangle,
+                                                                     const math::vec3 &normal,
+                                                                     const math::vec3 &point) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API math::vec3 FromBarycentricCoords(const Triangle &triangle,
+                                                                       const math::vec3 &bc) noexcept;
+
+    [[nodiscard]] ENGINE_GEOMETRY_API double SquaredDistance(const Triangle &triangle,
+                                                             const math::vec3 &point) noexcept;
 } // namespace engine::geometry
